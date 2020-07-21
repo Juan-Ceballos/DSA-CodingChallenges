09b91a90dff83ad7c96c2b7be6a79bc093f22319
Juan C <juanceballos@pursuit.org>
2020-07-20 22:21:39 -0400

    added solution for palindrome alphanumerice

--- DSA-CodingChallenges.playground/Pages/DSA-Ch2-IsPalindrome.xcplaygroundpage/Contents.swift
+++ DSA-CodingChallenges.playground/Pages/DSA-Ch2-IsPalindrome.xcplaygroundpage/Contents.swift
@@ -10,5 +10,22 @@ func isPalindrome(input: String) -> Bool {
 
 isPalindrome(input: "hello")
 isPalindrome(input: "rotator")
-
+isPalindrome(input: "A man, a plan, a canal: Panama")
 // mb try w/out using .reversed()
+
+func isPalindromeAdvanced(_ s: String) -> Bool  {
+    var arrS = Array(s.lowercased())
+    //print(arrS)
+    
+    for (index, element) in arrS.enumerated() {
+        if !element.isLetter && !element.isNumber   {
+            arrS[index] = Character(" ")
+        }
+    }
+    
+    let removeSpaces = String(arrS).replacingOccurrences(of: " ", with: "")
+    return String(removeSpaces.reversed()) == removeSpaces
+}
+
+isPalindromeAdvanced("Hello!")
+isPalindromeAdvanced("A man, a plan, a canal: Panama")
