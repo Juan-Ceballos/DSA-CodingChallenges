e575a1efac4e5bcfae7cedf7e5098154c1008388
Juan-Ceballos <juanceballos@pursuit.org>
2024-05-14 21:16:58 -0400

    added quick sort playground

---
+++ DSA-QuickSort.playground/Pages/QS-Freestyle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func partition(arr: inout [Int]) -> Int {
+    
+    var pivot = 0
+    var high = arr.count - 1
+    
+    while pivot > high {
+        let startVal = arr[pivot + 1]
+        let endVal = arr[high]
+        
+        //  
+    }
+    
+    
+    
+    
+    
+    return 0
+}
