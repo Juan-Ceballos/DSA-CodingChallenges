876a726882c682e58a1962c1b5ecb4be0c74d95b
Juan-Ceballos <juanceballos@pursuit.org>
2024-05-15 09:14:39 -0400

    freestyle learning of quicksort wrote partition code, yet to verify

--- DSA-QuickSort.playground/Pages/QS-Freestyle.xcplaygroundpage/Contents.swift
+++ DSA-QuickSort.playground/Pages/QS-Freestyle.xcplaygroundpage/Contents.swift
@@ -2,19 +2,34 @@ import Foundation
 
 func partition(arr: inout [Int]) -> Int {
     
-    var pivot = 0
+    var low = 0
     var high = arr.count - 1
+    var pivot = low + (high - low) / 2
     
-    while pivot > high {
-        let startVal = arr[pivot + 1]
-        let endVal = arr[high]
+    while low < high {
+        let lowVal = arr[low]
+        let highVal = arr[high]
+        let pivotVal = arr[pivot]
         
-        //  
+        if lowVal > pivotVal {
+            arr.swapAt(low, pivot)
+            low += 1
+        }
+        else if highVal < pivotVal {
+            arr.swapAt(high, pivot)
+            high -= 1
+        } else {
+            low += 1
+            high -= 1
+        }
     }
     
-    
-    
-    
-    
-    return 0
+    print(arr)
+    return arr[pivot]
 }
+
+var sampleArr = [5, 3, 2, 7, 1]
+
+let partitionVal = partition(arr: &sampleArr)
+
+print(partitionVal)
