08f0219dab523aa3965ca091a26fbe0a48ff5a86
Juan-Ceballos <juanceballos@pursuit.org>
2024-05-17 09:17:04 -0400

    practice qs

--- DSA-QuickSort.playground/Pages/QS-Freestyle.xcplaygroundpage/Contents.swift
+++ DSA-QuickSort.playground/Pages/QS-Freestyle.xcplaygroundpage/Contents.swift
@@ -1,9 +1,8 @@
 import Foundation
 
-func partition(arr: inout [Int]) -> Int {
-    
-    var low = 0
-    var high = arr.count - 1
+func partition(arr: inout [Int], low: Int, high: Int) -> Int {
+    var low = low
+    var high = high
     var pivot = low + (high - low) / 2
     
     while low < high {
@@ -24,12 +23,65 @@ func partition(arr: inout [Int]) -> Int {
         }
     }
     
-    print(arr)
-    return arr[pivot]
+    //print(arr)
+    return pivot
+}
+
+func quickSort(arr: inout [Int], low: Int, high: Int) {
+    /*
+     [1, 2, 3, 4, 5] - [], [], p?
+     
+     */
+    //split an array
+    //let low = 0
+    //let high = arr.count - 1
+    //while
+    if low < high {
+        let j = partition(arr: &arr, low: low, high: high)
+        quickSort(arr: &arr, low: low, high: j - 1)
+        quickSort(arr: &arr, low: j + 1, high: high)
+    }
+    
 }
 
 var sampleArr = [5, 3, 2, 7, 1]
 
-let partitionVal = partition(arr: &sampleArr)
+//let partitionVal = partition(arr: &sampleArr)
 
-print(partitionVal)
+//print(partitionVal)
+
+quickSort(arr: &sampleArr, low: 0, high: sampleArr.count - 1)
+print(sampleArr)
+
+//-----------------------------------------------------------------------------
+
+
+func partition2(arr: inout [Int], low: Int, high: Int) -> Int {
+    var i = low
+    var pivot = high
+    
+    for j in low..<high {
+        if arr[j] <= arr[pivot] {
+            arr.swapAt(i, j)
+            i += 1
+        }
+    }
+    
+    arr.swapAt(i, high)
+    return i
+}
+
+func quickSort2(arr: inout [Int], low: Int, high: Int) {
+    
+    if low < high {
+        var j = partition2(arr: &arr, low: low, high: high)
+        quickSort2(arr: &arr, low: low, high: j - 1)
+        quickSort2(arr: &arr, low: j + 1, high: high)
+    }
+}
+
+var sampleArr2 = [5, 3, 2, 7, 1]
+quickSort2(arr: &sampleArr2, low: 0, high: sampleArr2.count - 1)
+print(sampleArr2)
+
+
