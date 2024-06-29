96bfeac66f2d8c44e5e9e877ad99439a201caccf
Juan-Ceballos <juanceballos@pursuit.org>
2024-06-29 10:49:57 -0400

    bs fs 2

--- BinarySearch.playground/Pages/BinarySearch-FS.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/BinarySearch-FS.xcplaygroundpage/Contents.swift
@@ -25,3 +25,33 @@ var sampleArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 let found = binarySearch(arr: sampleArr, target: 3)
 print(found ?? -1)
+
+
+
+
+
+
+
+
+
+
+func binarySearch2(arr: [Int], target: Int) -> Int? {
+    
+    var left = 0
+    var right = arr.count - 1
+    
+    while left <= right {
+        let mid = left + (right - left) / 2
+        if arr[mid] == target {
+            return mid
+        } else if arr[mid] < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    
+    return nil
+}
+
+print(binarySearch2(arr: sampleArr, target: 7) ?? -1)
